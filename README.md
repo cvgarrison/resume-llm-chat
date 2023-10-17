0f55ad1184d380a60d0f13af771b14c04a1519c4
cvgarrison <52759030+cvgarrison@users.noreply.github.com>
2023-10-17 13:46:54 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Chat app based on LangChain + Next.js Template
+
+Visit https://langchain-starter-nine-eta.vercel.app/ and ask the robot to give information about me 😁
---
+++ app/retrieval/page.tsx
@@ -0,0 +1,27 @@
+import { ChatWindow } from "@/components/ChatWindow";
+
+export default function AgentsPage() {
+  const InfoCard = (
+    <div className="p-4 md:p-8 rounded bg-[#25252d] w-full max-h-[85%] overflow-hidden">
+      <h1 className="text-3xl md:text-4xl mb-4">
+        R.O.B the Retrieval Robot 🤖🔗
+      </h1>
+      <div>
+        I can answer any questions you have about Cooper and his resume!
+      </div>
+    </div>
+  );
+  return (
+    <ChatWindow
+      endpoint="api/chat/retrieval_agents"
+      emptyStateComponent={InfoCard}
+      showIngestForm={false}
+      showIntermediateStepsToggle={false}
+      placeholder={
+        'Beep boop! I\'m a robot retrieval-focused agent! Ask, "What is Cooper\'s job experience?"'
+      }
+      emoji="🤖"
+      titleText="R.O.B. the Retrieval Robot"
+    ></ChatWindow>
+  );
+}
